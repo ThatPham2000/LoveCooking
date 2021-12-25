20986d6e63457d65b1bfc7266efbe12a2f548d89
PHẠM VĂN THẬT <57900109+ThatPham2000@users.noreply.github.com>
2021-12-25 18:35:20 +0700

    add adapter gallery

--- app/src/main/java/com/nhom005/lovecooking/add/AddImageAndVideo.java
+++ app/src/main/java/com/nhom005/lovecooking/add/AddImageAndVideo.java
@@ -1,6 +1,7 @@
 package com.nhom005.lovecooking.add;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -12,9 +13,15 @@ import android.widget.ImageButton;
 import com.nhom005.lovecooking.MainActivity;
 import com.nhom005.lovecooking.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AddImageAndVideo extends Activity {
     ImageButton btnBack;
     Button btnNext;
+    RecyclerView recyclerView;
+    GalleryAdapter galleryAdapter;
+    List<Integer> listPhoto = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +30,8 @@ public class AddImageAndVideo extends Activity {
         btnBack = (ImageButton) findViewById(R.id.btn_back_to_home);
         btnNext = (Button) findViewById(R.id.btn_go_to_content);
 
+        recyclerView = (RecyclerView)findViewById(R.id.recycler_view_gallery);
+
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -30,6 +39,9 @@ public class AddImageAndVideo extends Activity {
                 AddImageAndVideo.this.overridePendingTransition(R.anim.right_to_left, R.anim.left_to_right);
             }
         });
+
+
+        galleryAdapter = new GalleryAdapter(AddImageAndVideo.this, listPhoto);
     }
 
     @Override
