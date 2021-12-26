a0ecf2e1e4083bed7d28777160b4b3fa4972d166
PHẠM VĂN THẬT <57900109+ThatPham2000@users.noreply.github.com>
2021-12-26 10:18:33 +0700

    trang nhập nội dung

---
+++ app/src/main/java/com/nhom005/lovecooking/add/AddContent.java
@@ -0,0 +1,42 @@
+package com.nhom005.lovecooking.add;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import com.nhom005.lovecooking.MainActivity;
+import com.nhom005.lovecooking.R;
+
+public class AddContent extends AppCompatActivity {
+    ImageButton btnBack;
+    Button btnPost;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_content);
+
+        btnBack = (ImageButton) findViewById(R.id.btn_back_to_add_image);
+        btnPost = (Button)findViewById(R.id.btn_post);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+                AddContent.this.overridePendingTransition(R.anim.default_status, R.anim.slide_out_right);
+            }
+        });
+
+        btnPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AddContent.this, MainActivity.class);
+                startActivity(intent);
+                AddContent.this.overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+            }
+        });
+    }
+}
--- app/src/main/java/com/nhom005/lovecooking/add/AddImageAndVideo.java
+++ app/src/main/java/com/nhom005/lovecooking/add/AddImageAndVideo.java
@@ -5,6 +5,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -97,6 +98,15 @@ public class AddImageAndVideo extends Activity {
                     recyclerView.setLayoutManager(gridLayoutManager);
                     recyclerView.setAdapter(galleryAdapter);
                 }
+            }
+        });
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AddImageAndVideo.this, AddContent.class);
+                startActivity(intent);
+                AddImageAndVideo.this.overridePendingTransition(R.anim.slide_in_right, R.anim.default_status);
             }
         });
     }
