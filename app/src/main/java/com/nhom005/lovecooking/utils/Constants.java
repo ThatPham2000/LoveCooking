fc079cb3defdd2641da7fe3dacf4998585f5ff1b
Phạm Minh Vương <47874420+windymv025@users.noreply.github.com>
2021-12-26 13:37:34 +0700

    update utils

--- app/src/main/java/com/nhom005/lovecooking/utils/Constants.java
+++ app/src/main/java/com/nhom005/lovecooking/utils/Constants.java
@@ -1,8 +1,6 @@
 package com.nhom005.lovecooking.utils;
 
-import com.nhom005.lovecooking.R;
 import com.nhom005.lovecooking.models.FeedNews;
-import com.nhom005.lovecooking.models.Story;
 import com.nhom005.lovecooking.models.User;
 
 import java.util.ArrayList;
@@ -11,4 +9,5 @@ public class Constants {
     public static ArrayList<User> historyUser = new ArrayList<>();
     public static ArrayList<String> historyTextSearch = new ArrayList<>();
     public static ArrayList<FeedNews> feedNews = new ArrayList<>();
+    public static ArrayList<User> users = new ArrayList<>();
 }
